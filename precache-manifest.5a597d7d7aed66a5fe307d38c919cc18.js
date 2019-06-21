576296ec1bedac9ad665ef917b239171dcdaa505
johniblake <johniblake93@gmail.com>
2019-06-21 14:26:34 -0500

    Updates

---
+++ precache-manifest.5a597d7d7aed66a5fe307d38c919cc18.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf613505535eb28545b480abc9398a6d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "473baf8fa74bd00a5b10",
+    "url": "/clicky-game/static/css/main.1077bd72.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/clicky-game/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "473baf8fa74bd00a5b10",
+    "url": "/clicky-game/static/js/main.ff879072.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "e267d5a97f82ac9a55361ba81ebe4cc1",
+    "url": "/clicky-game/static/media/MovementV.e267d5a9.ttf"
+  },
+  {
+    "revision": "5c69ef51cdeb115f27cb15ee160b5c43",
+    "url": "/clicky-game/static/media/arya.5c69ef51.jpg"
+  },
+  {
+    "revision": "601d03af48fbba496044207242baa86e",
+    "url": "/clicky-game/static/media/beric.601d03af.jpg"
+  },
+  {
+    "revision": "97c80bee4a6934569fc1a9961bc10ba7",
+    "url": "/clicky-game/static/media/brienne.97c80bee.jpg"
+  },
+  {
+    "revision": "b2af4a80addc507b425e8e3c0f32bc0a",
+    "url": "/clicky-game/static/media/dany.b2af4a80.jpg"
+  },
+  {
+    "revision": "82fd51a6a1ee5daa416245d3317aaad7",
+    "url": "/clicky-game/static/media/jon.82fd51a6.jpg"
+  },
+  {
+    "revision": "56a51209c326d168e94234835a2969ea",
+    "url": "/clicky-game/static/media/margaery.56a51209.jpg"
+  },
+  {
+    "revision": "ed5a39b1f484d091d3c93a456d23d4ba",
+    "url": "/clicky-game/static/media/oberyn.ed5a39b1.webp"
+  },
+  {
+    "revision": "94fcba00c364ad3e8837f08a33d7f8ee",
+    "url": "/clicky-game/static/media/petyr.94fcba00.png"
+  },
+  {
+    "revision": "d5b8c647894388cf73be5657a016d89c",
+    "url": "/clicky-game/static/media/sandor.d5b8c647.png"
+  },
+  {
+    "revision": "e91b8d42acbfc8d6b320b74661a4f057",
+    "url": "/clicky-game/static/media/sansa.e91b8d42.png"
+  },
+  {
+    "revision": "63789674e967328b52b45ddd9caa5e80",
+    "url": "/clicky-game/static/media/tyrion_cup.63789674.jpg"
+  },
+  {
+    "revision": "9b91f96638962e7b30673e47075b2e14",
+    "url": "/clicky-game/static/media/varys.9b91f966.jpg"
+  }
+]);
